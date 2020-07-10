5307b800d635a1cb8f8909de20192284c7ad6947
DESKTOP-2NLB9QJ\Krkt <mustafasacli70@gmail.com>
2020-07-11 02:36:05 +0300

    first step. Driver dll list added.

---
+++ Oracle/Queries/List_Of_Foreign_Keys_V6.sql
@@ -0,0 +1,43 @@
+/* Formatted on 14.1.2016 15:37:42 (QP5 v5.252.13127.32867) */
+WITH PK_LST
+     AS (  SELECT COLS.TABLE_NAME,
+                  COLS.COLUMN_NAME,
+                  COLS.POSITION,
+                  CONS.VALIDATED,
+                  CONS.STATUS,
+                  CONS.OWNER,
+                  CONS.CONSTRAINT_NAME
+             FROM ALL_CONSTRAINTS CONS
+                  INNER JOIN ALL_CONS_COLUMNS COLS
+                     ON     CONS.CONSTRAINT_NAME = COLS.CONSTRAINT_NAME
+                        AND CONS.OWNER = COLS.OWNER
+            WHERE     CONS.CONSTRAINT_TYPE = 'P'
+                  AND UPPER (CONS.STATUS) = 'ENABLED'
+                  AND UPPER (CONS.OWNER) = 'ISKUR'
+                  AND COLS.TABLE_NAME NOT LIKE '%$%'
+         ORDER BY COLS.TABLE_NAME, COLS.POSITION)
+  SELECT C.OWNER,
+         A.TABLE_NAME,
+         A.COLUMN_NAME,
+         A.CONSTRAINT_NAME,
+         -- referenced pk
+         C.R_OWNER,
+         C_PK.TABLE_NAME R_TABLE_NAME,
+         PK_LST.COLUMN_NAME AS R_COLUMN_NAME,
+         C_PK.CONSTRAINT_NAME R_PK,
+         C.CONSTRAINT_TYPE AS CTYP
+    FROM ALL_CONS_COLUMNS A
+         JOIN ALL_CONSTRAINTS C
+            ON A.OWNER = C.OWNER AND A.CONSTRAINT_NAME = C.CONSTRAINT_NAME
+         JOIN ALL_CONSTRAINTS C_PK
+            ON     C.R_OWNER = C_PK.OWNER
+               AND C.R_CONSTRAINT_NAME = C_PK.CONSTRAINT_NAME
+         JOIN PK_LST
+            ON     C_PK.CONSTRAINT_NAME = PK_LST.CONSTRAINT_NAME
+               AND C_PK.TABLE_NAME = PK_LST.TABLE_NAME
+   WHERE     1 = 1
+         AND C.CONSTRAINT_TYPE = 'R'
+         AND UPPER (C.OWNER) = 'ISKUR'
+         AND (   (A.TABLE_NAME LIKE UPPER ( :PTABLENAME))
+              OR (C_PK.TABLE_NAME LIKE UPPER ( :PTABLENAME)))
+ORDER BY A.CONSTRAINT_NAME
